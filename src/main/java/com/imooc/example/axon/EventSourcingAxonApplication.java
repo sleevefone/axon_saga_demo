af6fbc9c310d460ae6b860279f13a806de46d262
stevan <522400740@qq.com>
2021-09-05 20:04:02 +0800

    init

---
+++ src/main/java/com/imooc/example/axon/EventSourcingAxonApplication.java
@@ -0,0 +1,44 @@
+package com.imooc.example.axon;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.security.NoTypePermission;
+import org.axonframework.serialization.Serializer;
+import org.axonframework.serialization.xml.XStreamSerializer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+@SpringBootApplication
+
+public class EventSourcingAxonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventSourcingAxonApplication.class, args);
+	}
+
+//	/**
+//	 * https://stackoverflow.com/questions/55298592/axon-4-xstream-configuration
+//	 */
+//	@Bean
+//	XStream xstream(){
+//		XStream xstream = new XStream();
+//		// clear out existing permissions and set own ones
+//		xstream.addPermission(NoTypePermission.NONE);
+//		// allow any type from the same package
+//		xstream.allowTypesByWildcard(new String[] {
+//				"com.ourpackages.**",
+//				"org.axonframework.**",
+//				"java.**",
+//				"com.thoughtworks.xstream.**"
+//		});
+//
+//		return xstream;
+//	}
+//
+//	@Bean
+//	@Primary
+//	public Serializer serializer(XStream xStream) {
+//		return XStreamSerializer.builder().xStream(xStream).build();
+//	}
+}
---
+++ src/main/java/com/imooc/example/axon/SwaggerConfig.java
@@ -0,0 +1,73 @@
+package com.imooc.example.axon;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+
+/**
+ * https://blog.csdn.net/qq_40663787/article/details/106333663
+ *
+ *
+ *
+ */
+@Configuration
+@EnableOpenApi
+public class SwaggerConfig {
+
+
+    @Bean
+    public Docket docket1(Environment environment) {
+
+        // 设置了swagger的docket的环境
+        boolean flag = environment.acceptsProfiles(Profiles.of("dev", "test"));
+
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .groupName("HybridGaw1")
+                .enable(true)
+                .select()
+                // 指定要扫描的包
+                .apis(RequestHandlerSelectors.any())
+                .build();
+    }
+
+    @Bean
+    public Docket docket(Environment environment) {
+
+        // 设置了swagger的docket的环境
+//        boolean flag = environment.acceptsProfiles(Profiles.of("dev", "test"));
+
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .groupName("HybridGaw")
+                .enable(true)
+                .select()
+                // 指定要扫描的包
+                .apis(RequestHandlerSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        Contact contact = new Contact("Hybrid", "www.baidu.com", "1831938181@qq.com");
+
+        return new ApiInfo("Api文档", "Api文档",
+                "1.0", "www.baidu.com",
+                contact, "Apache 2.0",
+                "http://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList<VendorExtension>());
+    }
+}
+
+
+
