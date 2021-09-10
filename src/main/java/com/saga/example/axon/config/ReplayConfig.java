0986939536107a2d5f1c2618610de4bed2749946
sleevan <522400740@qq.com>
2021-09-10 18:01:47 +0800

    new save

--- src/main/java/com/saga/example/axon/config/ReplayConfig.java
+++ src/main/java/com/saga/example/axon/config/ReplayConfig.java
@@ -2,21 +2,37 @@ package com.saga.example.axon.config;
 
 import org.axonframework.config.Configuration;
 import org.axonframework.config.EventProcessingConfigurer;
+import org.axonframework.eventhandling.TrackedEventMessage;
+import org.axonframework.eventhandling.TrackingToken;
 import org.axonframework.eventhandling.pooled.PooledStreamingEventProcessor;
+import org.axonframework.messaging.StreamableMessageSource;
 
 import java.time.Duration;
+import java.util.function.Function;
 
 /**
  * @author fanhb on 2021/9/10
+ * @desc xxxx
  */
 public class ReplayConfig implements EventProcessingConfigurer.PooledStreamingProcessorConfiguration {
     @Override
 
     public PooledStreamingEventProcessor.Builder apply(Configuration config, PooledStreamingEventProcessor.Builder builder) {
 
-        return builder.initialToken(messageSource -> messageSource.createTokenSince(Duration.ofDays(31)));
+        return builder.initialToken(new Function<StreamableMessageSource<TrackedEventMessage<?>>, TrackingToken>() {
+                    @Override
+                    public TrackingToken apply(StreamableMessageSource<TrackedEventMessage<?>> messageSource) {
+
+                        //                            messageSource.openStream()
+//                                        messageSource.
+                        TrackingToken tokenSince = messageSource.createTokenSince(Duration.ofDays(31));
+                        //                            messageSource.openStream()
+                        return tokenSince;
+                    }
+                }
+        );
     }
-}
+};
 
 
 
--- src/main/java/com/saga/example/axon/customer/query/CustomerProjector.java
+++ src/main/java/com/saga/example/axon/customer/query/CustomerProjector.java
@@ -5,55 +5,74 @@ import com.saga.example.axon.customer.event.CustomerCreatedEvent;
 import com.saga.example.axon.customer.event.CustomerDepositedEvent;
 import com.saga.example.axon.customer.event.OrderPaidEvent;
 import org.axonframework.config.ProcessingGroup;
-import org.axonframework.eventhandling.AllowReplay;
 import org.axonframework.eventhandling.EventHandler;
+import org.axonframework.modelling.command.Aggregate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * Created by mavlarn on 2018/5/22.
  */
 @Service
 @ProcessingGroup("customerProjector")
-@AllowReplay
+//@AllowReplay
 public class CustomerProjector {
 
     @Autowired
-    private CustomerEntityRepository repository;
+    private CustomerEntityRepository jpaCustomerEntityRepository;
+
+    @Resource
+    private org.axonframework.modelling.command.Repository<CustomerEntity> axonRepository;
 
     @EventHandler
     public void on(CustomerCreatedEvent event) {
         CustomerEntity customer = new CustomerEntity(event.getCustomerId(), event.getName(), event.getPassword(), 0d);
-        repository.save(customer);
+        jpaCustomerEntityRepository.save(customer);
     }
 
     @EventHandler
     public void on(CustomerDepositedEvent event) {
         String customerId = event.getCustomerId();
-        CustomerEntity accountView = repository.getOne(customerId);
+        CustomerEntity accountView = jpaCustomerEntityRepository.getOne(customerId);
 
         Double newDeposit = accountView.getDeposit() + event.getAmount();
         accountView.setDeposit(newDeposit);
-        repository.save(accountView);
+        jpaCustomerEntityRepository.save(accountView);
     }
 
     @EventHandler
     public void on(CustomerChargedEvent event) {
-        String customerId = event.getCustomerId();
-        CustomerEntity customer = repository.getOne(customerId);
 
-        Double newDeposit = customer.getDeposit() - event.getAmount();
-        customer.setDeposit(newDeposit);
-        repository.save(customer);
+        Aggregate<CustomerEntity> one = axonRepository.load(event.getCustomerId());
+        one.execute(o->{
+            Double newDeposit = o.getDeposit() - event.getAmount();
+            o.setDeposit(newDeposit);
+            jpaCustomerEntityRepository.save(o);
+
+        });
+
+        jpaCustomerEntityRepository.findOne(Example.of(CustomerEntity.builder()
+                .id(event.getCustomerId())
+                .build()
+        )).ifPresent(o -> {
+            Double newDeposit = o.getDeposit() - event.getAmount();
+            o.setDeposit(newDeposit);
+            jpaCustomerEntityRepository.save(o);
+
+        });
+
     }
 
     @EventHandler
     public void on(OrderPaidEvent event) {
         String customerId = event.getCustomerId();
-        CustomerEntity customer = repository.getOne(customerId);
+        CustomerEntity customer = jpaCustomerEntityRepository.getOne(customerId);
 
         Double newDeposit = customer.getDeposit() - event.getAmount();
         customer.setDeposit(newDeposit);
-        repository.save(customer);
+        jpaCustomerEntityRepository.save(customer);
     }
 }
